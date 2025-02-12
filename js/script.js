d85d07082790236a53b22b77450a7704c32e3f42
Dennis Geerlof <d.geerlof.13@gmail.com>
2025-02-12 17:00:18 +0100

    home

--- js/script.js
+++ js/script.js
@@ -71,6 +71,35 @@
         window.location.href = `mailto:${email}?subject=${subject}&body=${body}`;
     }
 
+    function toonFormulier() {
+        document.getElementById("reservering-form").style.display = "block";
+    }
+    
+
+    // reserveer formulier
+    function verstuurReservering() {
+        const email = "hvclubhuis@gmail.com";
+        const naam = document.getElementById("naam").value;
+        const userEmail = document.getElementById("email").value;
+        const telefoon = document.getElementById("telefoon").value;
+        const datum = document.getElementById("datum").value;
+        const tijd = document.getElementById("tijd").value;
+    
+        if (!naam || !userEmail || !telefoon || !datum || !tijd) {
+            alert("Vul alle velden in voordat je verzendt.");
+            return;
+        }
+    
+        const subject = encodeURIComponent("Nieuwe Reservering");
+    
+        const body = encodeURIComponent(
+            `Naam: ${naam}\nE-mail: ${userEmail}\nTelefoonnummer: ${telefoon}\nDatum: ${datum}\nTijd: ${tijd}`
+        );
+    
+        window.location.href = `mailto:${email}?subject=${subject}&body=${body}`;
+    }
+ 
+
     // img pop-up
     document.querySelectorAll('img').forEach(img => {
         img.addEventListener('click', function () {
